51accea81478976989fc9d470a69d54750c0bda6
aucaland <aucaland@debian>
2024-05-17 10:15:04 -0400

    reboot vm internet failing

---
+++ setupVm.sh
@@ -0,0 +1,40 @@
+sudo apt update -y
+sudo apt upgrade -y
+
+sudo apt install make -y
+
+sudo apt install curl -y
+
+sudo apt install vim -y
+
+echo "VBOX INSTALLING.."
+wget -O- -q https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmour -o /usr/share/keyrings/oracle_vbox_2016.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle_vbox_2016.gpg] http://download.virtualbox.org/virtualbox/debian bookworm contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+sudo apt update
+sudo apt install virtualbox-7.0 -y
+
+echo "K3D INSTALLING.."
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+echo "DOCKER INSTALLING.."
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "VAGRANT INSTALLING.."
+sudo apt install vagrant-libvirt libvirt-daemon-system
+
+echo "ADD HOSTS"
+echo "192.168.56.110	app1.com"
+echo "192.168.56.110	app2.com"
