ab8df7a1cf14d986d4eb87a0399a49ff0b00860b
aucaland <aucaland@debian>
2024-05-19 07:16:30 -0400

    Deployment required value error on argoCD

--- p3/scripts/setup-cluster.sh
+++ p3/scripts/setup-cluster.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 BLUE='\033[0;34m'
 RED='\033[0;31m'
 RESET='\033[0m'
@@ -13,11 +14,14 @@ echo "${BLUE}ArgoCD installation..${RESET}"
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 echo "${BLUE}Waiting for pods..${RESET}"
 sleep 5
-sudo kubectl wait --for=condition=ready pod --all -n argocd --timeout=250s
+sudo kubectl wait --for=condition=ready pod --all -n argocd --timeout=300s
+
 echo "${BLUE}Initial ArgoCD secret:${RESET}"
 sudo kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 --decode
 echo
 
+echo "${BLUE}Applying ArogCD Application.."
+sudo kubectl apply -f ../confs/app.yaml
 echo "${BLUE}Listening for argoCD..${RESET}"
 sudo kubectl port-forward -n argocd svc/argocd-server 8080:443
 
