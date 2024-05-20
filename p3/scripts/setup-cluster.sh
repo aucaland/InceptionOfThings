b3a287399d797a4feca3ab73bcd463df7159fcb4
aucaland <aucaland@debian>
2024-05-20 09:26:34 -0400

    v1 to v2

--- p3/scripts/setup-cluster.sh
+++ p3/scripts/setup-cluster.sh
@@ -16,6 +16,17 @@ echo "${BLUE}Waiting for pods..${RESET}"
 sleep 5
 sudo kubectl wait --for=condition=ready pod --all -n argocd --timeout=300s
 
+echo "${BLUE}ArgoCD CLI installation..${RESET}"
+if command -v argocd &> /dev/null
+then
+    echo "${RED}ArgoCD CLI already installed${RESET}"
+else
+    echo "${BLUE}Installing ArgoCD CLI..${RESET}"
+    curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+    sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+    rm argocd-linux-amd64
+fi
+
 echo "${BLUE}Initial ArgoCD secret:${RESET}"
 sudo kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 --decode
 echo
@@ -23,5 +34,5 @@ echo
 echo "${BLUE}Applying ArogCD Application.."
 sudo kubectl apply -f /home/aucaland/Documents/IOT/p3/confs/app.yaml
 echo "${BLUE}Listening for argoCD..${RESET}"
-sudo kubectl port-forward -n argocd svc/argocd-server 8080:443
+sudo kubectl port-forward -n argocd svc/argocd-server 8080:443 &
 
